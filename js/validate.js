2898a1ca94feaed1f5e8675baa5f5f4d8f317d5b
USER-20151106WE\Administrator <zhongjianjun@zhujia360.com>
2017-08-02 14:39:56 +0800

    第一次提交

---
+++ js/validate.js
@@ -0,0 +1,34 @@
+//以下为修改jQuery Validation插件兼容Bootstrap的方法，没有直接写在插件中是为了便于插件升级
+$.validator.setDefaults({
+    highlight: function (element) {
+        $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+    },
+    success: function (element) {
+        element.closest('.form-group').removeClass('has-error').addClass('has-success');
+    },
+    errorElement: "span",
+    errorPlacement: function (error, element) {
+        if (element.is(":radio") || element.is(":checkbox")) {
+            error.appendTo(element.parent().parent().parent());
+        } else {
+            error.appendTo(element.parent());
+        }
+    },
+    errorClass: "help-block m-b-none",
+    validClass: "help-block m-b-none"
+
+});
+
+// 手机号码验证
+$.validator.addMethod("isMobile", function(value, element) {
+    var length = value.length;
+    var mobile = /^(13[0-9]{9})|(18[0-9]{9})|(14[0-9]{9})|(17[0-9]{9})|(15[0-9]{9})$/;
+    return this.optional(element) || (length == 11 && mobile.test(value));
+}, "请正确填写您的手机号码");
+
+//下拉框验证
+$.validator.addMethod("inputarea", function(value, element){
+    if(value == "请选择"){
+        return false;
+    }
+},"请选择下拉框");
